10f65c0c9ae6ec51a1c1c5b66fbc495b9cfb8041
Kevin2606 <andreskevin2606@gmail.com>
2023-08-17 11:03:15 -0500

    feat: :sparkles: Implementacion de express-acl

--- index.js
+++ index.js
@@ -2,44 +2,29 @@ import express from 'express';
 import dotenv from 'dotenv';
 import passportHelper from './helpers/passportHelpert.js';
 import { crearToken } from './middlewares/middlewareJWT.js';
+import acl from 'express-acl';
 
 dotenv.config();
+
+acl.config({
+    filename: 'nacl.json',
+    baseUrl: 'api',
+    roleSearchPath: 'user.rol',
+    denyCallback: (res) => res.status(403).json({
+      message: 'No tienes permisos para acceder a este recurso'
+    })
+});
+
 const app = express();
 
 app.use(passportHelper.initialize());
 
-// Crear un token con el usuario de la persona
 app.get('/token/:usuario', crearToken);
 
-// Proteger una ruta con autenticación Bearer
-/** 
- * ! Importante !
- * Antes de colocar el token colocar la palabra Bearer
- * En los headers agregar: {
- *      Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6Ik...
- * }
- */
-
-// Middleware para validar permisos de roles
-const rolesPermitidos = {
-    admin: ['admin', 'vendedor'],
-    vendedor: ['vendedor']
-}
-
-const validarPermisos = (req, res, next) => {
-  //Comprueba que el usuario este accediendo a la url permitida para su rol
-  if (rolesPermitidos[req.user.rol].includes(req.url.split('/')[2])) {
-    next();
-  } else {
-    res.status(403).send('No tienes permisos para acceder a este recurso');
-  }
-}
-// ------------------------------
-
-app.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
+app.get('/api/admin', passportHelper.authenticate('bearer', { session: false }), acl.authorize, (req, res) => {
     res.json({mensaje: 'Hola admin', usuario: req.user});
 });
-app.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), validarPermisos, (req, res) => {
+app.get('/api/vendedor', passportHelper.authenticate('bearer', { session: false }), acl.authorize, (req, res) => {
     res.json({mensaje: 'Hola vendedor', usuario: req.user});
 });
 
